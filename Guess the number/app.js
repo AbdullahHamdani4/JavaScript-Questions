2b36f8768f2481d1ce67233ee00a1cbd024c35c3
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2025-10-23 00:24:56 +0500

    Guess the number game

---
+++ Guess the number/app.js
@@ -0,0 +1,16 @@
+//Guess the Number
+let number=7;
+let user_number=+prompt("Guess the number between 1-10");
+if(user_number == number){
+    console.log("Congratulations You guessed right");
+}else if( user_number == number - 1 || user_number == number + 1){
+  console.log("Bas ek step dor");
+}else if(user_number==  number - 2 || user_number == number + 2){
+  console.log("Thoda sa or behtar guess kro");
+}else if(user_number==  number - 3 || user_number == number + 3){
+  console.log("Qareeb horahe ho kooshis krte raho mittar");
+}else if(user_number==  number - 4 || user_number == number + 4){
+  console.log("Kooshis krte raho");
+}else{
+  console.log("Tumse na hopaiga");
+}
