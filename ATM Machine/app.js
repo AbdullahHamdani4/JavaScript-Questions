7f48f441d55c7f8e7871336663a12cc7c66fc102
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2025-10-25 12:57:00 +0500

    New files

---
+++ ATM Machine/app.js
@@ -0,0 +1,38 @@
+//Atm Machine
+
+
+alert("Welcome to Bank Alfalah Atm Machine");
+
+let pincode = "1234";
+let userpin = "1234";
+
+let salaryagai;
+let kitnesalary;
+let withdraw;
+if (userpin == pincode) {
+
+    salaryagai = prompt("Salary agai (Please asnwer with yes or no)");
+    salaryagai = salaryagai.toLowerCase();
+    //Nested 1
+    if (salaryagai == "no") {
+        console.log("Insufficient Amount");
+    } else if (salaryagai == "yes") {
+        kitnesalary = prompt("Kitne salary hai?");
+        //Nested 2
+        if (kitnesalary < 999) {
+            console.log("You cannot withdraw as your salary is less than the required amount for withdrawl");
+        } else if (kitnesalary > 999) {
+            withdraw = prompt("How much you want?");
+            //Nested 3
+            if (withdraw > kitnesalary) {
+                console.log("Impossible Amount");
+            }
+            else if (withdraw) {
+                console.log(`You succesfully withdrawed ${withdraw}Rs and the remaining amount in the bank is ${kitnesalary - withdraw}`);
+            } else console.log("Enter withdraw amount");
+        }   
+    }
+
+} else console.log("Pin code incorrect");
+
+
