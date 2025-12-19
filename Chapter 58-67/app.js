999f17b215c47e176639eadecb6c955e1e1df42e
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2025-12-19 21:43:41 +0500

    Fix formatting of console log for parent node of email for consistency

--- Chapter 58-67/app.js
+++ Chapter 58-67/app.js
@@ -46,5 +46,5 @@ console.log("Previous sibling of lastName", lastName2.previousSibling);
 console.log("Next sibling of lastName", lastName2.nextSibling);
 
 //part vi
-console.log("Parent node of email", email.parentNode);
+console.log("Parent node of email ==> ", email.parentNode);
 console.log("Node type of email", email.nodeType);
